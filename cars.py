9604815a23b7ae6cdcc12ff7f64957473edcd582
Kurt Krafft <kkrafft6609@gmail.com>
2020-04-13 11:34:30 -0500

    challenges complete

---
+++ cars.py
@@ -0,0 +1,72 @@
+makes = (
+  (1, "Toyota"), (2, "Nissan"),
+  (3, "Ford"), (4, "Mini"),
+  (5, "Honda"), (6, "Dodge"),
+)
+
+models = (
+  (1, "Altima", 2), (2, "Thunderbird", 3),
+  (3, "Dart", 6), (4, "Accord", 5),
+  (5, "Prius", 1), (6, "Countryman", 4),
+  (7, "Camry", 1), (8, "F150", 3),
+  (9, "Civic", 5), (10, "Ram", 6),
+  (11, "Cooper", 4), (12, "Pilot", 5),
+  (13, "Xterra", 2), (14, "Sentra", 2),
+  (15, "Charger", 6)
+)
+
+colors = (
+  (1, "Black" ), (2, "Charcoal" ), (3, "Red" ), (4, "Brick" ),
+  (5, "Blue" ), (6, "Navy" ), (7, "White" ), (8, "Ivory" )
+)
+
+available_car_colors = (
+  (1, 1), (1, 2), (1, 7),
+  (2, 1), (2, 3), (2, 7),
+  (3, 2), (3, 3), (3, 7),
+  (4, 3), (4, 5), (4, 8),
+  (5, 2), (5, 4), (5, 8),
+  (6, 2), (6, 6), (6, 7),
+  (7, 1), (7, 3), (7, 7),
+  (8, 1), (8, 5), (8, 8),
+  (9, 1), (9, 6), (9, 7),
+  (10, 2), (10, 5), (10, 7),
+  (11, 3), (11, 6), (11, 8),
+  (12, 1), (12, 4), (12, 7),
+  (13, 2), (13, 6), (13, 8),
+  (14, 2), (14, 5), (14, 8),
+  (15, 1), (15, 4), (15, 7)
+)
+cars = {}
+
+for make in makes:
+    cars[make[1]] = {}
+    for model in models:
+        if model[2] == make[0]:
+            cars[make[1]][model[1]] = []
+
+for model in models:
+    for available_color in available_car_colors:
+        if available_color[0]== model[0]:
+            for color in colors:
+                if color[0]== available_color[1]:
+                    for make in makes:
+                        if make[0]== model[2]:
+                            cars[make[1]][model[1]].append(color[1])
+
+# print(cars)
+
+for car in cars.items():
+    print(f"{car[0]}")
+    print('------------')
+    for model in car[1].items():
+        # print(model)
+        string = ",".join(model[1])
+        print(f"the {model[0]} is available in {string}")
+        print("-")
+        print("-")
+        print(" ")
+
+
+                     
+
